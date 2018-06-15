3c07d4dc7954863a3d674adcc632a66d7d726646
Vishal-Chintalapati <Vishal-Chintalapati@users.noreply.github.com>
2018-06-16 01:59:03 +0530

    Updated with Upload and Download for Beneficiary and O and M

--- CGSolar/Models/OperationAndMaintenanceModel.cs
+++ CGSolar/Models/OperationAndMaintenanceModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 
 namespace CGSolar.Models
 {
@@ -9,16 +10,34 @@ namespace CGSolar.Models
     {
         public string WorkOrderNo { get; set; }
         public string BeneficiaryName { get; set; }
+        public int BeneficiaryID { get; set; }
         public string Village { get; set; }
 
         public string Block { get; set; }
         public string District { get; set; }
         public string SystemCapacity { get; set; }
         public string Contact { get; set; }
+        public int AssignedTo { get; set; }
+        public string AssignedEmployee { get; set; }
         public DateTime? ReportedDate { get; set; }
         public string ProblemDescription { get; set; }
         public DateTime? CompletionDate { get; set; }
         public string Status { get; set; }
+
+
+        // This will convert the passed XYZ object to JSON string
+        public static string Serialize(List<OperationAndMaintenanceModel> onMModel)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(onMModel);
+        }
+
+        // This will convert the passed JSON string back to XYZ object
+        public static List<OperationAndMaintenanceModel> Deserialize(string data)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<List<OperationAndMaintenanceModel>>(data);
+        }
 
     }
 }
